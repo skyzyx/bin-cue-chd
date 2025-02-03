6f68bd5a4dcce74b2a66706687cff20c7c5f27e7
Ryan Parman <ryan@ryanparman.com>
2025-02-03 01:13:58 -0700

    Make .cue lookup case insensitive.

--- convert.sh
+++ convert.sh
@@ -7,7 +7,7 @@ echo ""
 read -p "Press enter to continue. Press Ctrl+C to quit." x
 cd "$1" || exit
 
-"$(brew --prefix findutils)/bin/gfind" "$1" -type f -name "*.cue" -print0 | while IFS= read -r -d '' file; do
+"$(brew --prefix findutils)/bin/gfind" "$1" -type f -iname "*.cue" -print0 | while IFS= read -r -d '' file; do
     echo "Processing file: $file"
     "$(brew --prefix rom-tools)/bin/chdman" createcd -i "$file" -o "${file%.*}.chd"
 done
